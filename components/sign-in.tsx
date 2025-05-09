b98ff7c45389092054507c3cb5df87126e026949
Drchanmp <drcklinn@protonmail.com>
2025-05-09 10:56:33 +0630

    Update sign-in.tsx

--- components/sign-in.tsx
+++ components/sign-in.tsx
@@ -9,6 +9,14 @@ export default function SignIn() {
       }}
     >
       <button type="submit">Signin with GitHub</button>
-    </form>
+   
+        "use client"
+
+import { signIn } from "next-auth/react"
+ 
+export default function SignIn() {
+  return <button onClick={() => signIn("github")}></button>
+}
+</form>
   )
 } 
